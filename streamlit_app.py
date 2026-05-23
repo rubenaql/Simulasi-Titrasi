c3a9b665b4b6ff41a2520251faedbfcc4865d647
Ruben Aqilla <ruben.aqilla123@gmail.com>
2026-05-24 00:02:28 +0700

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,8 +0,0 @@
-import os
-import runpy
-
-# Entry point deploy
-# Pastikan path Simulasi.py di-load relatif terhadap folder ini.
-_here = os.path.dirname(os.path.abspath(__file__))
-runpy.run_path(os.path.join(_here, "Simulasi.py"), run_name="__main__")
-
