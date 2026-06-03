9cbba034251e86f28482c4ceda9d176b7bd1fbf5
Ruben Aqilla <ruben.aqilla123@gmail.com>
2026-06-03 15:18:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import runpy
 # Entry point deploy
 # Pastikan path Simulasi.py di-load relatif terhadap folder ini.
 _here = os.path.dirname(os.path.abspath(__file__))
-runpy.run_path(os.path.join(_here, "Simulasi.py"), run_name="__main__")
+runpy.run_path(os.path.join(_here, "Simulasi_v3.py"), run_name="__main__")
 
